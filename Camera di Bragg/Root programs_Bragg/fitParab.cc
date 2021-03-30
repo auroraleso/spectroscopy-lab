10a3aa4a887e4c42b8e299276bd7fc7b24ac972b
auroraleso <lesoaurora@gmail.com>
2021-03-30 17:23:03 +0200

    added zeeman stuff, modified plot about bragg

--- Camera di Bragg/Root programs_Bragg/fitParab.cc
+++ Camera di Bragg/Root programs_Bragg/fitParab.cc
@@ -22,12 +22,12 @@
 
 
 
-double_t fitfretta(double_t *x, double_t *par) 
+double_t fitfparab(double_t *x, double_t *par) 
 {
-   double_t fitval2 = par[0] + par[1]*x[0];
+   double_t fitval2 = par[0] + par[1]*x[0] + par[2]*x[0]*x[0];
   return fitval2;
 }
-double fitzero(double *x, double *par) {
+double fitzero3(double *x, double *par) {
   double fitval = par[0] + par[1]*0;
    
   return fitval;
@@ -35,13 +35,13 @@ double fitzero(double *x, double *par) {
 
  
 
-void lin() {
+void par() {
   //crea un puntatore e nel costruttore gli passiamo direttamente il nome del file da cui prendere i dati
   TGraphErrors *g = new TGraphErrors("thirdpeak.txt");
     // associo un nome al seti di dati, do' un nome al puntatore del TGraphErrors
   g->SetName("fit");
 
-  TCanvas * c1 = new TCanvas("c1", "fit calibrazione ",
+  TCanvas * c1 = new TCanvas("c1", "fit calibrazione larghezza vs radq(E)",
                              27, 50, 1020, 760);
   g->SetMarkerStyle(8);
   g->SetMarkerSize(1);
@@ -49,40 +49,43 @@ void lin() {
   //  "ap" significa che deve disegnare anche gli assi ( a sta per assi) e che deve disegnare i punti con un marker i cui parametri sono impostati precedentemente (p per i punti)
   g->Draw("AP");
   g->SetTitle("calib");
-  g->GetXaxis()->SetTitle("p mb ");
-  g->GetYaxis()->SetTitle(" peak ");
+  g->GetXaxis()->SetTitle("Efficiency ");
+  g->GetYaxis()->SetTitle(" E (keV) ");
   
   // definisco funzione del fit
   // la chiamo f1 e la associo alla funzione esponenziale che ho creato prima di nome fitf2. 
   //Devo dare i parametri xmin e xmax
-  TF1 *f1 = new TF1("f1", fitfretta, 300, 800, 2);
+  TF1 *f1 = new TF1("f1", fitfparab, 0, 10000, 3);
   f1->SetLineStyle(1);
     f1->SetLineColor(2);
     f1->SetLineWidth(1);
   // setta i valori iniziali dei parametri
-  f1->SetParameters(4060,0.13);
+  f1->SetParameters(0,0,0);
     // (nome parametro 0, nome parametro 1, nome parametro2)
-  f1->SetParNames("par0", "par1");
+  f1->SetParNames("par0", "par1", "par2" );
 
   
   g->Fit(f1,"RS");
   double p0= f1->GetParameter(0);
     double p1 = f1->GetParameter(1);
+       double p2 = f1->GetParameter(2);
  // prendo l'errore del fit che mi hp0dato sul parametro
   double ep0= f1->GetParError(0);
     double ep1 = f1->GetParError(1);
+     double ep2 = f1->GetParError(2);
   
   //stampo questi valori che ho recuperato
-  std::cout << "\nParametri retta "
-            << p0<< " +- " << ep0
-            << p1<< " +- " << ep1
+  std::cout << "\nParametri parabola "
+            << p0<< " +- " << ep0<< " "
+            << p1<< " +- " << ep1 << " "
+            << p2<< " +- " << ep2
             << std::endl;
 
           
 TMultiGraph* mg = new TMultiGraph();
     mg->Add(g);
     mg->Draw("ap");
-    mg->SetTitle("Energy vs p");
+   mg->SetTitle("Energy vs p");
     mg->GetXaxis()->SetTitle("p (mb)");
     mg->GetYaxis()->SetTitle("energy peak");
     f1->Draw("same");
@@ -114,7 +117,7 @@ TMultiGraph* mg = new TMultiGraph();
 
 
   
-  TF1 *zero = new TF1("zero", fitzero, 0, 10000, 2);
+  TF1 *zero = new TF1("zero", fitzero3, 0, 10000, 2);
     zero->SetLineStyle(9);
     zero->SetLineColor(7);
    TMultiGraph* mg1 = new TMultiGraph();
